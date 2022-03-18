90984b58a1c22e86d01636bd0deffcb8665329c4
Durrel Ganados <dmganados@gmail.com>
2022-03-18 23:13:57 +0800

    done working with responsive cards

--- src/components/AppNavBar.js
+++ src/components/AppNavBar.js
@@ -4,16 +4,16 @@ import { Link } from 'react-router-dom';
 
 function AppNavBar() {
 	return(
-		<Navbar id="navbar" expand="lg">
+		<Navbar id="navigationBar" expand="lg">
 			<Container>
 				<Navbar.Brand>E-commerce App</Navbar.Brand>	
-					<Navbar.Toggle aria-controls="basic-navbar-nav" />
-					<Navbar.Collapse>						
-						<Nav className="ml-auto navLists">							
-							<Nav.Link to="/" className="nav-link">Home</Nav.Link>
-							<Nav.Link>Catalog</Nav.Link>
-							<Nav.Link>Register</Nav.Link>
-							<Nav.Link >Login</Nav.Link>							
+					<Navbar.Toggle aria-controls="basic-navbar-nav"  />
+					<Navbar.Collapse id="basic-navbar-nav">						
+						<Nav className="ml-auto d-block navLists">							
+							<Nav.Link to="/" className="nav-link d-block d-sm-inline d-md-block d-lg-inline navOptions">Home</Nav.Link>
+							<Nav.Link className="nav-link d-block d-sm-inline d-md-block d-lg-inline navOptions">Catalog</Nav.Link>
+							<Nav.Link className="nav-link d-block d-sm-inline d-md-block d-lg-inline navOptions">Register</Nav.Link>
+							<Nav.Link className="nav-link d-block d-sm-inline d-md-block d-lg-inline navOptions">Login</Nav.Link>				
 						</Nav>						
 					</Navbar.Collapse>
 			</Container>
--- src/components/Themes.js
+++ src/components/Themes.js
@@ -7,27 +7,27 @@ import airpods from './banner/airpods.jpg'
 export default function Theme() {
 	return(
 		<Container>
-			<Row className="my-3 cardPosition">
-				<Col xs={12} md={4}>
+			<Row className="p-2 mb-5 d-flex align-items-lg-start align-items-md-start align-items-sm-end cardPosition">
+				<Col xs={12} md={4} className="mb-4 mt-lg-0 mt-sm-5 align-items-end">
 					<Card className="p-4 cardHighlight">
 						<Card.Body>
 							<Col>
-							<Card.Img src={phone} className="img-fluid mb-2 mr-2 p-1 d-inline gadgetImage1" />
-							<Card.Img src={vReality} className="img-fluid mb-2 p-1 d-none d-sm-none d-md-inline d-lg-inline gadgetImage2" width="150" />
+							<Card.Img src={phone} className="img-fluid mb-2 mr-2 p-1 d-inline d-sm-inline d-md-inline d-lg-inline gadgetImage1" />
+							<Card.Img src={vReality} className="img-fluid mb-2 p-1 d-inline d-sm-inline d-md-inline d-lg-inline gadgetImage2" width="150" />
 							</Col>
 							<Col>
-							<Card.Img src={airpods} className="img-fluid d-none d-sm-none d-md-inline d-lg-inline mr-2 p-1 gadgetImage3" />
-							<Card.Img src={controller} className="img-fluid d-none d-sm-none d-md-inline d-lg-inline p-2 gadgetImage4" />							
+							<Card.Img src={airpods} className="img-fluid d-inline d-sm-inline d-md-inline d-lg-inline mr-2 p-1 gadgetImage3" />
+							<Card.Img src={controller} className="img-fluid d-inline d-sm-inline d-md-inline d-lg-inline p-2 gadgetImage4" />							
 							</Col>
 
-							<Card.Title className="pt-4">Phones and Gadgets</Card.Title>
+							<Card.Title className="pt-4 fw-bolder">Phones and Gadgets</Card.Title>
 							<Card.Text >
 								See more		
 							</Card.Text>
 						</Card.Body>
 					</Card>					
 				</Col>
-				<Col xs={12} md={4}>
+				<Col xs={12} md={4} className="mb-4">
 					<Card className="p-4 cardHighlight">
 						<Card.Body>
 							<Card.Title>E-commerce</Card.Title>
@@ -37,7 +37,7 @@ export default function Theme() {
 						</Card.Body>
 					</Card>					
 				</Col>
-				<Col xs={12} md={4}>
+				<Col xs={12} md={4} className="mb-4">
 					<Card className="p-4 cardHighlight">
 						<Card.Body>
 							<Card.Title>E-commerce</Card.Title>
