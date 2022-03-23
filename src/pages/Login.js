582659722eda120ecab518acb2d25eb81394854b
Durrel Ganados <dmganados@gmail.com>
2022-03-23 16:55:29 +0800

    working on login authorization

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,18 +1,18 @@
 
-import { useState, useEffect, useContext } from 'react';
-import UserContext from '../UserContext';
+import { useState, useEffect } from 'react';
 import { Form, Button, Container, Card, Row, Col } from 'react-bootstrap';
 import Swal from 'sweetalert2';
-import { Navigate } from 'react-router-dom';
+
 
 
 export default function Login() {
 
-	const { user, setUser } = useContext(UserContext);
 	const [email, setEmail] = useState('');
 	const [password, setPassword] = useState('');
-	let addressSign = email.search('@');
-	let dns = email.search('.com');
+
+	let addressSign = email.search('@')
+	let dns = email.search('.com')
+
 	const [isActive, setIsActive] = useState(false);
 	const [isValid, setIsValid] = useState(false);
 
@@ -28,59 +28,41 @@ export default function Login() {
 			setIsValid(false);
 			setIsActive(false);
 		}
-	},[email, password, addressSign, dns])
+	})
 
-	const loginUser = async (event) => {
+	const loginUser = (event) => {
+		event.preventDefault();
+
 		fetch('https://limitless-brushlands-90925.herokuapp.com/users/user-token', {
 			method: 'POST',
 			headers: {
-				'Content-Type' : 'application/json'
+				'Content-Type': 'application/json'
 			},
 			body: JSON.stringify({
-				email : email,
+				email: email,
 				password: password
-			}).then(res => res.json()).then(tokenData => {
-				let token = tokenData.accessToken;
-				if (typeof token !== 'undefined') {
-					localStorage.setItem('accessToken', token);
-
-					fetch('https://limitless-brushlands-90925.herokuapp.com/users/users', {
-						headers: {
-							Authorization: `Bearer ${token}`
-						}
-					}).then(res => res.json()).then(convertedData => {
-						if (typeof convertedData._id !== "undefined") {
-							setUser({
-								id: convertedData._id,
-								isAdmin: convertedData.isAdmin
-							});
-							Swal.fire({
-								icon: 'success',
-								title: 'Login Successful',
-								text: 'Welcome'
-							})
-						} else {
-							setUser({
-								id: null,
-								isAdmin: null
-							})
-						}
-					});
-				} else {
-					Swal.fire({
-						icon: 'error',
-						title: 'Check your Credentials',
-						text: 'Credential Error'
-					})
-				}		
 			})
-		})
-	};
+		}).then(res => res.json()).then(jsonData => {
+			let token = jsonData.accessToken;
+			// console.log(typeof token);
 
-	return (
-		user.id ?
-			<Navigate to="/catalog" replace={true} />
-		:
+			if (typeof token !== 'undefined') {
+				Swal.fire({
+					icon: 'success',
+					title: 'Login Successful',
+					text: 'Welcome'
+				})
+			} else {
+				Swal.fire({
+					icon: 'error',
+					title: 'Check your Credentials',
+					text: 'Credential error'
+				})
+			}
+		})		
+	}
+
+	return (	
 		<>
 		<Container >
 			<h3 className="d-flex justify-content-center">Login to your Account</h3>
@@ -102,16 +84,18 @@ export default function Login() {
 								<h6 className="text-success">Email is Valid</h6>
 							:
 								<h6 className="text-mute">Email is Invalid</h6>
-						}
+						}						
 					</Form.Group>
 
 					{/*Password Field*/}
+
+					
 					<Form.Group>
 						<Form.Label>Password:</Form.Label>
 						<Form.Control 
 						type="password"
 						placeholder="Enter Password Here"
-						required
+						required	
 						value={password}
 						onChange={e => {setPassword(e.target.value)}}
 						/>
@@ -127,10 +111,11 @@ export default function Login() {
 						:
 							<Button
 							className="btn-block"
-							type="secondary"
-							>Login</Button>
-					}
-					
+							variant="secondary"
+							disabled
+							>Login</Button>	
+					}				
+													
 				</Form>
 			</Card>	
 		</Container>
