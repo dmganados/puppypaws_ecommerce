d2b1df11487d12cb6d425a5a962a2baf67bd3d83
Durrel Ganados <dmganados@gmail.com>
2022-04-11 23:41:12 +0800

    working on create product page

--- src/components/CreateProduct.js
+++ src/components/CreateProduct.js
@@ -1,54 +1,26 @@
-import { useState } from 'react';
+import { useState, useEffect, useContext } from 'react';
 import { Container, Row, Col, Form, Button, Dropdown } from 'react-bootstrap';
 import styled from 'styled-components';
+import Swal from 'sweetalert2';
 
-const Main = styled("div")`
-	font-family: sans-serif;
-	// height: 100vh;
-`;
-
-const DropDownContainer = styled("div")`
-	border: 2px solid #e5e5e5;
-	width: 100%;
-	height: 45px;
-`;
-
-const DropDownHeader = styled("div")``;
-const DropDownListContainer = styled("div")``;
-
-const DropDownList = styled("ul")`
-	padding: 0;
-	margin: 0;	
-	background: #909CC2;
-	border: 2px solid #e5e5e5;	
-	&:first-child {
-	    padding-top: 0.8em;
-	}
-`;
-const ListItem = styled("li")`
-	list-style: none;
-	margin-bottom: 0.8em;
-`;
-
-const options = ["Phone", "Shirt", "Electronics"]
 
 export default function CreateProduct() {
 
-	const [isOpen, setIsOpen] = useState(false);
-	const [selectedOption, setSelectedOption] = useState(null);
+	const newProduct = (submit) => {
 
-	const toggling = () => setIsOpen(!isOpen);
-
-	const onOptionClicked = value => () => {
-		setSelectedOption(value);
-		setIsOpen(false);
-		console.log(selectedOption);
-	};
+		return(
+			Swal.fire({
+				icon: 'success',
+				title: 'New Product is Created'
+			})
+		)
+	}
 
 	return(
 		<>
 		<Container>
-			<Form className="p-5">
+		<Col className='p-5'>
+			<Form onSubmit={e => newProduct(e)}>
 				<Form.Group>
 					<Form.Label>Product Name</Form.Label>
 					<Form.Control type="text" required />
@@ -65,37 +37,17 @@ export default function CreateProduct() {
 				</Form.Group>
 
 				<Form.Group>
-				<Form.Label>Category</Form.Label>
-				<Main>
-				<DropDownContainer>
-					<DropDownHeader onClick={toggling}>{selectedOption || "Select a Category"}</DropDownHeader>
-					{isOpen && (
-						<DropDownListContainer>
-							<DropDownList>
-								{options.map(option => (
-								<ListItem onClick={onOptionClicked(option)} key={Math.random()}>{option}</ListItem>
-								))}
-							</DropDownList>
-						</DropDownListContainer>
-					)}					
-				</DropDownContainer>
-				</Main>
-				</Form.Group>
-
-				<Form.Group>
 					<Form.Label>Stock</Form.Label>
 					<Form.Control type="number" required />
 				</Form.Group>
 
 				<div className="mb-4">
-					<input type="checkbox" /> Display product on the Catalog
+					<input type="checkbox" /> Display product as Active
 				</div>
 
 				<Button className="createBtn">Create Product</Button>
 			</Form>
-
-			
-			
+		</Col>			
 		</Container>
 		</>
 	)
