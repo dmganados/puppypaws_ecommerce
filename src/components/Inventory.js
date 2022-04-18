f3a4296824c83d18079273b11851644b59c419af
Durrel Ganados <dmganados@gmail.com>
2022-04-18 23:18:42 +0800

    working on inventory cards

--- src/components/Inventory.js
+++ src/components/Inventory.js
@@ -1,18 +1,20 @@
 import { Card, Row, Col, Container } from 'react-bootstrap'
+import { Link } from 'react-router-dom'
 
 export default function Inventory() {
 
 	return(
 		<div>
 			<Card className="cardInventory">
-				<Card.Body>
-					<Card.Title></Card.Title>
-					<Card.Text></Card.Text>
-				</Card.Body>
-
-			</Card>
-		
-	
+				<Card.Body>				
+					<Card.Title>Product Name</Card.Title>
+					<Card.Text>Description</Card.Text>
+					<Card.Text>Selling Price: PHP</Card.Text>
+					<Card.Text className="d-inline">Status:</Card.Text>				
+					<Link to="" className="mr-4" id="editFunction">Edit</Link>
+					<Link to="" id="deleteFunction">Delete</Link>
+				</Card.Body>				
+			</Card>	
 		</div>
 	)
 }
