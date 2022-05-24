bccc0f73cc0f1da03360722d28f08cdea23405e4
Durrel Ganados <dmganados@gmail.com>
2022-05-24 22:22:41 +0800

    done working on the option to upload a new image

--- src/pages/Edit.js
+++ src/pages/Edit.js
@@ -13,83 +13,67 @@ export default function Edit() {
 	let [description, setDescription] = useState('');
 	let [sellingPrice, setSellingPrice] = useState('');
 	let [stock, setStock] = useState('');	
-	let [productImg, setProductImg] = useState();	
+	let [productImg, setProductImg] = useState('');	
 	let [isActive, setIsActive] = useState(false);	
 	let [newFile, setNewFile] = useState([])
 	let [updatedProduct, setUpdatedProduct] = useState('')
 	let toggleChecked = () => setIsActive(value => !value)
 	let imageurl = updatedProduct.productImg;
 	imageurl = newFile.productImg
-	// console.log(updatedProduct.productImg)
-	// console.log(updatedProduct)
 
 	useEffect(() => {
-		productInfo();	
-		// newImage();	
+		productInfo();				
 	},[])
 
 	// Get the product details
 	const productInfo = async () => {
-		await fetch(`http://localhost:8000/products/${id}`).then(res => res.json()).then(data => {
-			// console.log(data)
+		await fetch(`http://localhost:8000/products/${id}`).then(res => res.json()).then(data => {			
 			setProductName(data.productName);
 			setDescription(data.description);
 			setSellingPrice(data.sellingPrice);
 			setStock(data.stock);
-			setIsActive(data.isActive);	
+			setIsActive(data.isActive);				
 		})
 	};
 
-	// const newImage = async () => {
-	// 	let addFile = new FormData();
-	// 	addFile.append('productImg', productImg)
-	// 	await fetch('http://localhost:8000/products/upload',{
-	// 		method: 'POST',
-	// 		body: addFile
-	// 	}).then(result => result.json()).then(file => {
-	// 		console.log(file)
-	// 	})
-	// }
-
-	const productUpdate = async () => {
-		// console.log(productName, description, sellingPrice, stock)
-		let formData = new FormData()
-		let addFile = new FormData();
-		addFile.append('productImg', productImg)
+	const productUpdate = async () => {		
+		let formData = new FormData()		
 		formData.append('productName', productName);
 		formData.append('description', description);
 		formData.append('sellingPrice', sellingPrice);
 		formData.append('stock', stock);
 		formData.append('isActive', isActive);
-		formData.append('productImg', newFile);
-		
-		// Condition if user changes the image
-		if (productImg) {
-			 fetch('http://localhost:8000/products/upload',{
-				method: 'POST',
-				body: addFile
-			}).then(result => result.json()).then(file => {
-				// setProductImg(file)
-				setNewFile(file)
-			})
-		};	
-		
-				
-		// Work on changing the product image by the new uploaded image
-		// Work on Delete Component
-		// Work on viewing individual product
-		// Work on Displaying Images on the Catalog
+		formData.append('productImg', productImg);
 
-		// Update the product details
-		let isUpdated = await fetch(`http://localhost:8000/products/${id}/update-product`, {
-			method: 'PUT',			
-			body: formData
-		}).then(res => res.json()).then(newData => {
-			setUpdatedProduct(newData)
-		})
+		// Created two API to let user choose to upload a new image or not
+		if (productImg) {
+			 fetch(`http://localhost:8000/products/${id}/update-product`, {
+				method: 'PUT',			
+				body: formData
+			}).then(res => res.json()).then(newData => {
+				console.log(newData)
+			})
+		} else {
+			fetch(`http://localhost:8000/products/${id}/update-no-image`, {
+				method: 'PUT',
+				headers: {
+					'Content-Type' : 'application/json'
+				},
+				body: JSON.stringify({
+					productName: productName,
+					description: description,
+					sellingPrice: sellingPrice,
+					stock: stock,
+					isActive: isActive
+				})
+			}).then(res => res.json()).then(newData => {
+				console.log(newData)
+			})
+		}		
 	};
 
-
+	// Work on the effects when done filling out the form (Swal effect)
+	// Work on disabling and enabling button when filling out the form
 
 	return (
 		<div>
@@ -151,7 +135,8 @@ export default function Edit() {
 					
 
 					<input 
-					type="file"					 
+					type="file"	
+					// value={productImg}		 
 					onChange={e => setProductImg(e.target.files[0])}
 					 /> <br/><br/>
 					 {/*<Button onClick={e => newImage(e)} >Upload</Button>*/}
