4cc9e02c8b9459cfd151ff5183894d80cc6fda0c
Durrel Ganados <dmganados@gmail.com>
2022-06-09 22:17:26 +0800

    working on tables

--- src/pages/ManageProduct.js
+++ src/pages/ManageProduct.js
@@ -13,16 +13,27 @@ import { Link } from 'react-router-dom';
 import Inventory from '../components/Inventory';
 import Edit from '../pages/Edit';
 import { useState, useEffect } from 'react';
+import { useTable } from 'react-table';
 
 
 export default function ManageProduct() {
 
 	const [inventoryCollection, setInventoryCollection] = useState([]);	
-	let [inventory, setInventory] = useState('');	
+	let [inventory, setInventory] = useState([]);	
 	let userCredentials = localStorage.accessToken;
 	let status = inventory.isActive
 	let listStatus = status === true ? 'Active' : 'Inactive';
-	// console.log(inventory[].isActive);
+	const columns = [
+		{
+			Header: 'Product Name',
+			accessor: 'productName'
+		},
+		{
+			Header: 'Price',
+			accessor: 'sellingPrice'
+		}
+	]
+	// console.log(inventory)
 
 	// Create a table for the inventory
 	// Get the boolean
@@ -37,19 +48,15 @@ export default function ManageProduct() {
 			headers: {
 				Authorization: `Bearer ${userCredentials}`
 			}
-		}).then(res => res.json()).then(inventoryData => {	
-			setInventory(inventoryData)
-			// console.log(inventoryData)
-			// if (!unmounted) {
-			// 	setInventoryCollection(inventoryData.map(inventory => {						
-			// 		// return(									
-			// 		// 	<Inventory key={inventory._id} inventoryProp={inventory} />										
-			// 		// )
-			// 	}));
-			// }		
-			// return () => {
-			// 	unmounted = true;
-			// }			
+		}).then(res => res.json()).then(inventoryData => {			
+			if (!unmounted) {
+				setInventoryCollection(inventoryData.map(inventoryInfo => {	
+					setInventory(inventoryInfo);
+				}));
+			}		
+			return () => {
+				unmounted = true;
+			}			
 		});
 	},[]);
 
@@ -57,19 +64,25 @@ export default function ManageProduct() {
 		<>
 			<Container>			
 				<Button className="mt-5 createBtn" href="/create-product">Create Product +</Button>
-				{inventoryCollection}	
-				<Table className="table mt-3">
-					<thead>
-						<tr>
-							
+
+				
+				{/*{inventoryCollection}	*/}
+				{/*<Table className="table mt-3">*/}
+					{/*<thead>
+						<tr>							
 							<th scope="col">Product Name</th>							
 							<th scope="col">Selling Price</th>
 							<th scope="col">Stock</th>
 							<th scope="col">Status</th>
 							<th scope="col">Update/Delete</th>
 						</tr>
-					</thead>
-					<tbody>
+					</thead>*/}
+					{/*<tbody>*/}
+						
+						{/*<tr>
+							<td>{inventory.productName}</td>
+							<td>${inventory.sellingPrice}</td>
+						</tr>*/}
 						{/*{inventory.map((item) => (
 							<tr>
 								<td>{item.productName}</td>
@@ -78,8 +91,8 @@ export default function ManageProduct() {
 							))}
 */}
 
-						{/*
-						{inventory.map((items) => ( 
+						
+						{/*{inventory.map((items) => ( 
 						<tr>						
 							<td>{items.productName}</td>							
 							<td>${items.sellingPrice}</td>
@@ -89,8 +102,8 @@ export default function ManageProduct() {
 						</tr>
 						))}*/}
 						
-					</tbody>
-				</Table>					
+					{/*</tbody>*/}
+				{/*</Table>					*/}
 			</Container>
 
 		</>
